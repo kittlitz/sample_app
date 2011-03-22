dfc5065e8679eb94d3f21f14549d83e099b46a75
Ken Kittlitz <kittlitz@gmail.com>
2011-03-22 16:06:43 -0400

    Added a User factory and test

---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+Factory.define :user do |user|
+  user.name                  "Ken Kittlitz"
+  user.email                 "kittlitz@gmail.com"
+  user.password              "foobar"
+  user.password_confirmation "foobar"
+end
