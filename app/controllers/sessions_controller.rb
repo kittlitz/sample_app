f0f4ea82558a1153052cc53be4c2fe642630cde0
Ken Kittlitz <kittlitz@gmail.com>
2011-03-23 13:17:12 -0400

    Handle invalid signin

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,17 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.authenticate(params[:session][:email],
+                             params[:session][:password])
+    if user.nil?
+      # Tell flash to only appear on current render, rather than on
+      # next request.
+      flash.now[:error] = "Invalid email/password combination"
+      @title = "Sign in"
+      render 'new'
+    else
+      # handle success.
+    end
   end
 
   def destroy
